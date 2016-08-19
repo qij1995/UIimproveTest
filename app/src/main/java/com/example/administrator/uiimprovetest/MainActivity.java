d71a7daf98d5c607688959fb8b19725632d31a75
Eric <425190711@qq.com>
2016-08-19 10:27:19 +0800

    little UI about fragment,dialog and gridview

--- app/src/main/java/com/example/administrator/uiimprovetest/MainActivity.java
+++ app/src/main/java/com/example/administrator/uiimprovetest/MainActivity.java
@@ -1,33 +1,49 @@
 package com.example.administrator.uiimprovetest;
 
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-private ImageButton photo;
+    private ImageButton photo;
+    private EditText edit;
+    private RelativeLayout line1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        GridViewFragment fragment = new GridViewFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frag_layout, fragment);
-        transaction.commit();
-        photo=(ImageButton)findViewById(R.id.photo);
+//        在动态调用fragment的时候会用到下面的方法
+//        GridViewFragment fragment = new GridViewFragment();
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.frag_layout, fragment);
+//        transaction.commit();
+        photo = (ImageButton) findViewById(R.id.photo);
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
                 startActivity(intent);
             }
         });
+//        edit=(EditText)findViewById(R.id.edit1) ;
+//        line1=(RelativeLayout)findViewById(R.id.line1);
+//        edit.setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//                 TODO Auto-generated method stub
+//                edit.setCursorVisible(true);
+//                edit.setFocusable(true);
+//                edit.setFocusableInTouchMode(true);
+//                edit.requestFocus();
+//                return true;
+//            }
+//      });
     }
 }
